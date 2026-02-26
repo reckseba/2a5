97cb9d055f30c12021a012f4fa1ca5d57bfecf53
Sebastian <16629355+reckseba@users.noreply.github.com>
2026-02-26 13:20:43 +0100

    Upgrade all other api dependencies

--- api/cypress.config.js
+++ api/cypress.config.js
@@ -12,6 +12,7 @@ const clientConfig = {
 };
 
 module.exports = defineConfig({
+    allowCypressEnv: false,
     e2e: {
         baseUrl: "http://localhost:3000",
         setupNodeEvents(on, config) {
